57150a9dcdb0dbc62304344b0ecd17116cab2bb6
Thomas Larsen <toften@gmail.com>
2013-04-15 21:40:26 +0100

    Adding Maven mojo

---
+++ src/main/java/net/toften/docmaker/maven/DocMakerMojo.java
@@ -0,0 +1,16 @@
+package net.toften.docmaker.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo ( name = "docmaker" )
+public class DocMakerMojo extends AbstractMojo {
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
