ca5e1cef8247a4718b105c5252aa9da2906d7d0d
Thomas Larsen <toften@gmail.com>
2013-11-03 22:32:37 +0000

    Added a BaseSection class, that all section types should derive from, instead of the MetaSection class

---
+++ src/main/java/net/toften/docmaker/BaseSection.java
@@ -0,0 +1,14 @@
+package net.toften.docmaker;
+
+public class BaseSection {
+	private String sectionName;
+
+	public BaseSection(String sectionName) {
+		this.sectionName = sectionName;
+	}
+	
+	public String getSectionName() {
+		return sectionName;
+	}
+
+}
