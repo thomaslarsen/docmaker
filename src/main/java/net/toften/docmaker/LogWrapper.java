9351b5a33dc5693e03edbe08b1226b11d1879c32
Thomas Larsen <toften@gmail.com>
2015-03-08 23:31:51 +0000

    Adding NullLogWrapper, which does nothing

--- src/main/java/net/toften/docmaker/LogWrapper.java
+++ src/main/java/net/toften/docmaker/LogWrapper.java
@@ -1,6 +1,21 @@
 package net.toften.docmaker;
 
 public interface LogWrapper {
+	public static final class NullLogWrapper implements LogWrapper {
+
+		@Override
+		public void debug(String message) {
+		}
+
+		@Override
+		public void info(String message) {
+		}
+
+		@Override
+		public void warn(String message) {
+		}
+	}
+	
 	void debug(String message);
 	
     void info(String message);
